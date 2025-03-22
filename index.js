2d665fbecefd6942ba94f828bc6770d51418de0d
tnfqkdrpwjd <tnfqkdrpwjd@gmail.com>
2025-03-23 02:33:30 +0900

    p를 div로 바꿈

--- index.js
+++ index.js
@@ -13,6 +13,104 @@ function getFile() {
       let parser = new DOMParser();
       let logFile = parser.parseFromString(e.target.result, "text/html"); // HTML 문자열을 DOM 객체로 변환
 
+      let characters = new Map();
+      let taps = new Map();
+      // HTML 수정 작업
+      logFile.querySelectorAll("p").forEach((p) => {
+        // p 태그 내부의 span 태그 찾기
+        let spans = p.querySelectorAll("span");
+        if (spans.length >= 3) {
+          //탭의 []지우기기
+          let tap = spans[0].outerText.slice(2, -1);
+          //저널의 색 가져오기
+          let characterStyle = p.getAttribute("style");
+
+          //해싱하기 - 이름, 탭을 그대로 클레스로 쓸 수 없음
+          let tapH = base32Decode(spans[0].outerText, "t");
+          let characterH = base32Decode(spans[1].outerText, "c");
+
+          //div태그를 만들고 클래스 추가
+          let newDiv = document.createElement("div");
+          newDiv.setAttribute("class", `${tapH} ${characterH}`);
+
+          //이름
+          let divChar = document.createElement("div");
+          divChar.setAttribute("class", "character");
+          divChar.textContent = spans[1].outerText;
+          //대사
+          let divDia = document.createElement("div");
+          divDia.setAttribute("class", "dialogue");
+          divDia.textContent = spans[2].outerText;
+          //이미지
+          let divImg = document.createElement("div");
+          divImg.setAttribute("class", "img");
+          let imgChar = document.createElement("img");
+          imgChar.setAttribute("alt", `${spans[1].outerText} 이미지`);
+          divImg.appendChild(imgChar);
+
+          newDiv.appendChild(divChar);
+          newDiv.appendChild(divDia);
+
+          logFile.body.appendChild(newDiv);
+          //탭 지우기
+          p.remove();
+          //맵에 추가하기
+          characters.set(characterH, characterStyle);
+          taps.set(tapH, tap);
+        }
+
+        //p태그의 style 속성 제거
+        p.removeAttribute("style");
+      });
+      let style = document.createElement("style");
+
+      characters.forEach((value, key, map) => {
+        style.innerHTML += `.${key} { ${value}} 
+        `;
+      });
+
+      logFile.head.appendChild(style);
+      // //파일 내용 그대로 출력력
+      // document.getElementById("filedata").innerText =
+      //   logFile.documentElement.outerHTML;
+
+      // html로 표시하기
+      document.getElementById("htmlContent").innerHTML =
+        logFile.documentElement.outerHTML;
+
+      // //복사
+      // let htmlString = new XMLSerializer().serializeToString(logFile); // 다시 HTML 문자열로 변환
+
+      // navigator.clipboard
+      //   .writeText(htmlString)
+      //   .then(() => {
+      //     alert("HTML이 클립보드에 복사되었습니다!");
+      //   })
+      //   .catch((err) => {
+      //     console.error("복사 실패:", err);
+      //   });
+    };
+
+    reader.onerror = function () {
+      console.error("파일을 읽는 중 오류가 발생했습니다.");
+    };
+
+    reader.readAsText(file); // 파일 내용을 텍스트로 읽기
+  } else {
+    alert("파일을 선택해주세요.");
+  }
+}
+
+function getFileOld() {
+  let fileInput = document.getElementById("logfile");
+  let file = fileInput.files[0]; // 선택된 파일 가져오기
+
+  if (file) {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let parser = new DOMParser();
+      let logFile = parser.parseFromString(e.target.result, "text/html"); // HTML 문자열을 DOM 객체로 변환
+
       let styles = new Map();
       // HTML 수정 작업
       logFile.querySelectorAll("p").forEach((p) => {
@@ -31,7 +129,9 @@ function getFile() {
           //p 태그에 클래스 추가
           let tap = base32Decode(spans[0].outerText, "t");
           let character = base32Decode(spans[1].outerText, "c");
+
           p.setAttribute("class", tap + " " + character);
+
           //span 태그에 character인지 dialogue 인지 class 로 구분
           spans[1].setAttribute("class", "character");
           spans[2].setAttribute("class", "dialogue");
